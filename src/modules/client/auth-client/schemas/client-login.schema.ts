244a4d3c295ebdb0770f997e8f509fd0bc524ad3
Viet Anh <anhlv.ce190674@gmail.com>
2026-02-02 20:31:28 +0700

    [20260202][AnhLV] update-login-schema

--- src/modules/client/auth-client/schemas/client-login.schema.ts
+++ src/modules/client/auth-client/schemas/client-login.schema.ts
@@ -1,8 +1,30 @@
-import { z } from "zod"
+import { z } from "zod";
+import customersData from '@/assets/customer.json';
+
+const customers = customersData.customers;
 
 export const loginSchema = z.object({
-    email: z.string().min(1,"Input your email").email("Invalid email format"),
-    password: z.string().min(8,"Password required 8 characters"),
+    email: z.string().min(1, "Input your email").email("Invalid email format"),
+    password: z.string().min(6, "Password required 6 characters"),
+}).superRefine((data, ctx) => {
+    const customer = customers.find(u => u.email === data.email);
+
+    if (!customer) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Email is not registered!",
+            path: ["email"],
+        });
+        return; 
+    }
+
+    if (customer.password_hash !== data.password) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Wrong password!",
+            path: ["password"],
+        });
+    }
 });
 
-export type LoginFormValues = z.infer<typeof loginSchema>
+export type LoginFormValues = z.infer<typeof loginSchema>;
