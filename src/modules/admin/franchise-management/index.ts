220072c25c004ae4a3eed9b0f672318093c63f01
Quocanh9 <truongquocanh04vl@gmail.com>
2026-02-03 12:27:58 +0700

    [02032026][ANHTQ]-test-github

--- src/modules/admin/franchise-management/index.ts
+++ src/modules/admin/franchise-management/index.ts
@@ -1,3 +1,5 @@
 export { default as FranchiseListPage } from './pages/FranchiseListPage';
 export { default as FranchiseApplicationListPage } from './pages/FranchiseApplicationListPage';
 export { default as FranchiseManagement } from './pages/FranchiseManagement';
+// testing export
+// check github
