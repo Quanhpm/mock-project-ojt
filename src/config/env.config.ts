e86a2b88110344d3c2d37092c67e7cab6ae0f09a
Đỗ Minh Phú <phudo1508@gmail.com>
2026-03-21 18:42:51 +0700

    test1

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-  API_URL: import.meta.env.VITE_API_URL,
+  API_URL: import.meta.env.VITE_API_URL || 'https://ecommerce-franchise-training-nodejs.vercel.app/api',
   CLOUDINARY_CLOUD_NAME: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || 'de2dyvcb7',
   CLOUDINARY_UPLOAD_PRESET: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || 'OJT_MOCKPROJECT',
 } as const;
